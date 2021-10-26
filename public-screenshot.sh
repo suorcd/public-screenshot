b0c33b49e0f5d1727302b26d28bca5552cf8a3b5
suorcd <60907848+suorcd@users.noreply.github.com>
2021-10-26 14:23:28 -0700

    Moving vars to their own file

--- public-screenshot.sh
+++ public-screenshot.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-DATE=$(date +'%Y%m%d%H%M%S')
-FIL="$(echo "${DATE}${RANDOM}" | sha256sum - | awk '{print $1}').png"
-SSHSRV="gaz"
-SRV="files.example.com"
-RFIL="/srv/example/srv/$SRV/$FIL"
-FILEURL="https://$SRV/$FIL"
-RFIL="/srv/${SRV}/${FIL}"
-FILEURL="https://${SRV}/${FIL}"
+
+source ./public-screenshot.env
+
 
 cd "${HOME}/Pictures" || ( echo -n "Couldn't 'cd' into ~/Pictures" | xclip -selection c && exit 1 )
 gnome-screenshot --file="${FIL}"
